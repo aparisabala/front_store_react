c245e542f7f524ffbfdad0410ce137deaebdf084
Paris Bala <arianbala56@gmail.com>
2026-02-12 18:05:08 +0600

    react added

--- src/app/Lib/render/renderPageThree.js
+++ src/app/Lib/render/renderPageThree.js
@@ -3,7 +3,7 @@ import DatingZone from './../../Components/DatingZone';
 const renderPageThree = [
   {
     Component: DatingZone,
-    query: 'getdata/getProducts?pid=34&page=1&1mit=50&channel=h001',
+    query: 'getdata/getProducts?pid=35&page=1&limit=80&channel=h001',
   }
 ];
 export default renderPageThree;
